cb9240781435c3fa63d1c5d88a060c95c5415d2d
Grudzio <23323916+Grudzio@users.noreply.github.com>
2024-05-12 10:09:25 +0200

    poprawa pliku main.js

--- js/main.js
+++ js/main.js
@@ -9,13 +9,12 @@ app.post('/login', (req, res) => {
     const password = req.body.password;
 
     // Tutaj powinna nastąpić weryfikacja danych logowania, np. porównanie z danymi w bazie danych
-
     if (username === 'admin' && password === 'password') {
-        res.send('Zalogowano pomyślnie!');
+        res.json({ message: 'Zalogowano pomyślnie!' });
     } else {
-        res.send('Błędne dane logowania!');
+        res.json({ message: 'Błędne dane logowania!' });
     }
-});
+    });
 
 app.listen(3000, () => {
     console.log('Serwer działa na porcie 3000');
